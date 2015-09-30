9da4da176471272abeaad58efb9a82ba7561d818
vishal simon <vishlasimon20121981@gmail.com>
2015-09-30 17:41:14 +0530

    ip.txt fixes
    fixes related to ip.

--- app/src/main/java/services/OnBootReceiver.java
+++ app/src/main/java/services/OnBootReceiver.java
@@ -6,16 +6,31 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import tasks.CommandTask;
+import utils.Utils;
 
 public class OnBootReceiver extends BroadcastReceiver {
+
+    private static final String TAG = OnBootReceiver.class.getSimpleName();
+
+    private static String htdocs = Environment.getExternalStorageDirectory() + File.separator + "htdocs";
     @Override
     public void onReceive(Context context, Intent intent) {
     	
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-        	
+
+            checkHtdocsOK();
+            writeIpAddress("http://"+"localhost:8080");
         	final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
             if (preferences.getBoolean("enable_server_on_boot", false)) {
                 
@@ -34,6 +49,35 @@ public class OnBootReceiver extends BroadcastReceiver {
         }
         if (intent.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)) {
             BackgroundIntentService.performAction(context, BackgroundIntentService.ACTION_PACKAGE_REMOVED);
+        }
+    }
+
+
+    public boolean checkHtdocsOK() {
+        File file = new File(htdocs);
+        if (file.exists()) {
+            return true;
+        }
+        return file.mkdirs();
+
+    } // END checkHtdocsOK
+
+    private void writeIpAddress(String ipAddress){
+
+        File ipFile = new File(htdocs,"IP.txt");
+        try {
+            FileOutputStream f = new FileOutputStream(ipFile);
+            PrintWriter pw = new PrintWriter(f);
+            pw.print(ipAddress);
+            pw.flush();
+            pw.close();
+            f.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.i(TAG, "******* File not found. Did you" +
+                    " add a WRITE_EXTERNAL_STORAGE permission to the   manifest?");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
