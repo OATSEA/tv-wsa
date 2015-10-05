98bdecf4dfc0b04aa694c46e95f5a863754c6f76
Vishal Simon <vishalsimon20121981@gmail.com>
2015-10-05 17:56:21 +0530

    change in date of teacher virus

--- app/src/main/assets/getinfected.php
+++ app/src/main/assets/getinfected.php
@@ -1480,10 +1480,10 @@ if($_SESSION['isValidation']['flag'] == 1)
                 <input type="button" name="button" id="button" value="GO!" align="center" onclick="checkLoaded(true);">  
             </div><br/>
             <div class="full-widthdebug">
-                <div class="mandatory">V: 0.4 | B: master | TS: 20151002.0100</div>
+                <div class="mandatory">Getinfected - V: 0.4 | B: master | TS: 20151002.0100</div>
             </div>
             <div class="full-widthdebug">
-                <div class="mandatory">V: 0.4 | B: master | TS: TS: 20151002.0100</div>
+                <div class="mandatory">Teacher Virus - V: 0.4 | B: master | TS: 20151005.0555</div>
             </div>
         </div>
         <input type="hidden" name="setting_value" id="setting_value">
